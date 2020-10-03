fb0364910b7bdba44f974c6f6db0baecca816775
sandianyue <35618994+sandianyue@users.noreply.github.com>
2020-10-03 20:55:16 +0800

    add helloworld

--- OFFICIAL_OP.sh
+++ OFFICIAL_OP.sh
@@ -11,11 +11,12 @@
 #
 
 # Uncomment a feed source
-sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 sed -i '$a src-git pass https://github.com/huchanghui123/Lienol-openwrt-packages-backup' feeds.conf.default
 sed -i '$a src-git X https://github.com/x-wrt/com.x-wrt' feeds.conf.default
+sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
 sed -i '$a src-git lean https://github.com/coolsnowwolf/lede/' feeds.conf.default
 #git clone https://github.com/MeIsReallyBa/x-wrt/tree/master/target
