e131851ed483c1733467f0c01ad2ea4eb9d8a0a5
sandianyue <35618994+sandianyue@users.noreply.github.com>
2020-10-04 11:16:11 +0800

    65535

--- OFFICIAL_OP_PLUS.sh
+++ OFFICIAL_OP_PLUS.sh
@@ -12,6 +12,7 @@
 
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+sed -i 's/16384/65535/g' package/kernel/linux/files/sysctl-nf-conntrack.conf
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
