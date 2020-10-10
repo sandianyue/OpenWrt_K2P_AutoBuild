57656a776416c7a76350821b19602d2759ea8ef7
sandianyue <35618994+sandianyue@users.noreply.github.com>
2020-10-10 22:37:41 +0800

    fix replace ss

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,7 +13,7 @@
 # Modify default IP
 sed -i 's/192.168...1/192.168.2.1/g' package/base-files/files/bin/config_generate
 sed -i 's/zh-cn is not set/zh-cn=y/g' .config
-sed -i 's/libmbedtls/libopenssl/g'  feeds/packages/shadowsocks-libev/Makefile
+sed -i 's/libmbedtls/libopenssl/g'  feeds/packages/net/shadowsocks-libev/Makefile
 sed -i 's/libmbedtls/libopenssl/g'  feeds/pass/lienol/luci-app-passwall/Makefile
 sed -i 's/luci-theme-bootstrap/argon/g' feeds/luci/collections/luci/Makefile
 ls -d `pwd`/package/lean/luci-app-*/po/zh-cn | xargs -L 1 -n 1 -i sh -c "ln -s {} \$(dirname {})/zh-Hans"
